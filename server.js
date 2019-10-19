d53d8ed299efe2e639a6f047803dd00b5d44bdc8
imvamsi <krrishvamsi7@gmail.com>
2019-10-18 21:45:17 -0400

    added express graphQL server setup :construction:

--- server.js
+++ server.js
@@ -11,5 +11,6 @@ app.use(
   })
 );
 
+//runs on PORT during deployment else runs on 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
