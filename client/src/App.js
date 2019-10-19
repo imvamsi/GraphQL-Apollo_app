4d967205d2d72fd7921a8b33dcc84de6f205fc04
imvamsi <krrishvamsi7@gmail.com>
2019-10-19 14:11:47 -0400

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import spacexlogo from "./spacexlogo.png";
 import "./App.css";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql"
+  uri: "/graphql"
 });
 
 function App() {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 const cors = require("cors");
+const path = require("path"); //core nodejs module
 const app = express();
 
 //allow cross-origin
@@ -15,6 +16,12 @@ app.use(
   })
 );
 
+//statis folder
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 //runs on PORT during deployment else runs on 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
